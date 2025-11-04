fa20de6b31f9a68c1573da08409f1bf81d989b3d
Abdurakhmon2 <abdurakhmon1721@gmail.com>
2025-11-04 09:42:53 +0900

    ...

--- payment service/controllers/payment.controller.ts
+++ payment service/controllers/payment.controller.ts
@@ -1,4 +1,4 @@
-import { AuthMiddleware } from "@/middlewares/auth.middleware";
+import { Authenticate, AuthMiddleware, Authorized } from "@/middlewares/auth.middleware";
 import { PaymentService } from "@/services/payment.service";
 import { PaymentGateway, StripePayment } from "@/utils/payment";
 import { BodyParams, PathParams, UseBefore } from "@tsed/common";
@@ -6,17 +6,18 @@ import { Controller, Inject } from "@tsed/di";
 import { Get, Post } from "@tsed/schema";
 
 @Controller('/payment')
-@UseBefore(AuthMiddleware)
 export class PaymentController {
     @Inject()
     private paymentService!: PaymentService
     
     @Post('/create-payment')
+    @Authorized(Authenticate())
     async createPayment(@BodyParams('orderNumber') orderNumber: string) {
         return await this.paymentService.createPayment(orderNumber)
     }
 
     @Get('/verify-payment/:id')
+    @Authorized(Authenticate())
     async verifyPayment(@PathParams('id') id: string) {
         return await this.paymentService.verifyPayment(id)
     }
--- payment service/middlewares/auth.middleware.ts
+++ payment service/middlewares/auth.middleware.ts
@@ -1,28 +1,64 @@
-import { validateUser } from '@/utils/broker/api'
-import { Context, Middleware, MiddlewareMethods, Next, Req, Res } from '@tsed/common'
-import { Unauthorized } from '@tsed/exceptions'
+import { Context, Middleware, MiddlewareMethods, Req, Res, UseAuth } from "@tsed/common";
+import { Request } from "express";
+import { Forbidden, Unauthorized } from '@tsed/exceptions'
+import { validateUser } from "@/utils/broker";
+import { USER_ROLE } from "@/modules/declarations";
+import { useDecorators } from '@tsed/core'
+import { In, Returns } from '@tsed/schema'
 
 @Middleware()
 export class AuthMiddleware implements MiddlewareMethods {
-    async use(
-        @Req() request: Req,
-        @Context() ctx: Context,
-        @Next() next: Next
-    ) {
-        const token = request.headers.authorization
+    public async use(@Req() request: Req, @Context() ctx: Context) {
+        const options = ctx.endpoint.get(AuthMiddleware) || {}
 
-        if (!token) {
-            throw new Unauthorized('Unauthorised: auhtorizaton token missing!')
+        await getUser(request)
+
+        if (!request.user) {
+            throw new Unauthorized('Unauthorized')
         }
 
-        if (!token?.startsWith("Bearer ")) {
-          throw new Unauthorized("Unauthorised: auhtorizaton token missing!");
+        if (request.user.role === USER_ROLE.ADMIN) {
+            return true
         }
 
-        const userData = await validateUser(token)
-        ctx.set('user', userData)
-
-        request.user = userData
-        next()
+        if (options.role) {
+            if (!request.user.role === options.role) {
+                throw new Forbidden('You are not authorized to access this resourse')
+            }
+        }
     }
+}
+
+async function getUser(request: Request) {
+    const token = request.token
+    if (!token) {
+        throw new Forbidden('Unauthorized')
+    }
+
+    const user = await validateUser(token)
+
+    if (!user) {
+        throw new Unauthorized('Unauthorized')
+    }
+
+    request.user = user
+    request.user.isSuper = user.role === USER_ROLE.SUPER_ADMIN
+    request.user.isAdmin = request.user.isSuper || user.role === USER_ROLE.ADMIN
+}
+
+export type RoleRequirements = {
+    role: USER_ROLE
+}
+
+export function Authenticate(role: USER_ROLE = USER_ROLE.USER): RoleRequirements {
+    return {role}
+}
+
+export function Authorized(options: RoleRequirements) {
+    return useDecorators(
+        UseAuth(AuthMiddleware, options),
+        In('header').Name('Authorization').Type(String).Required(true),
+        Returns(401).Description('Unauthorized'),
+        Returns(403).Description('Forbidded')
+    )
 }
